d058c3560fd52439c43d56ab2bc7d46552395fd1
Aryan shukla <113294396+Aryanshukla206@users.noreply.github.com>
2024-04-29 00:55:10 +0530

    Update morphology.py

--- morphology.py
+++ morphology.py
@@ -2,7 +2,7 @@ import streamlit as st
 import cv2
 import numpy as np
 from PIL import Image
-
+# Morphological Operations for Binary Images
 def dilation(image, kernel_size=(3, 3)):
     kernel = np.ones(kernel_size, np.uint8)
     return cv2.dilate(image, kernel, iterations=1)
